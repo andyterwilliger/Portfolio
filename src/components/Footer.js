d0953db26e600f6f342dfe9d7c92ba7c1c684396
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-02-25 13:41:01 -0600

    styling and hover styling for footer icons. Links added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,25 @@ function Footer() {
                 <div id='github-i'>
                     <ul id = 'icons-ul'>
                         <div>
-                            <li>
-                                <FontAwesomeIcon icon={faGithub} size='2x' />
+                            <li >
+                            <a target = '_blank' href= 'https://github.com/andyterwilliger'>
+                                <FontAwesomeIcon icon={faGithub} size='2x' className='icon'  />
+                            </a>
                             </li>
                         </div>
                         <div id='linkedin-i'>
                             <li>
-                            <FontAwesomeIcon icon={faLinkedin} size='2x' />
+                            <a target = '_blank' href= 'https://www.linkedin.com/in/andy-terwilliger/'>
+                            <FontAwesomeIcon icon={faLinkedin} size='2x' className='icon' />
+                            </a>
                             </li>
                         </div>
                         <div id = 'stack-i'>
                             <li>
-                                <FontAwesomeIcon icon= {faStackOverflow} size= '2x'/>
+                            <a target = '_blank' href= 'https://stackoverflow.com/users/18311028/andy123'>
+
+                                <FontAwesomeIcon icon= {faStackOverflow} size= '2x' className='icon'/>
+                                </a>
                             </li>
 
                         </div>
