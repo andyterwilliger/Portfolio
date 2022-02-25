97a868cc2de4b64b189b018ad682542709ecd38f
Andy Terwilliger <andy@terwilliger1@gmail.com>
2022-02-25 10:25:16 -0600

    add form to contact page, connect it with Formspring api and styling

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,23 @@
 function About(props){
     return(
+        <div id = 'about-body'>
         <h1>About</h1>
+        <div id = 'about-img-container'>
+        <img id='about-img' src="https://i.imgur.com/QxQPz1H.jpg" alt="profile-img"/>
+        </div>
+       <div id = 'about-blurb'>
+            <p>
+                Well hello there! My name is Andy Terwilliger and I am a developer living in 
+                Chicago, Illinois. I am passionate, disciplined, and driven by a desire to learn 
+                as much as I can possibly get my hands on. 
+            </p>
+            
+            <p>
+                My path to becoming a developer has certainly been an interesting one, considering just last 
+
+            </p>
+        </div>
+        </div>
     )
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,37 @@
 function Contact(props){
 
     return(
-        <div>
+        <div id = 'contact-body'>
             <h1>Contact Page</h1>
+            <section>
+            <div id ='form-container'>
+            <form action="https://formspree.io/f/mvolaeyj"
+             method= 'POST'
+            >
+                <div className = 'form-group'>
+                <label for="firstName">First Name: </label>
+                <input type="text" name="firstName" id="firstName"/>
+                </div>
+                <div className = 'form-group'>
+                <label for="lastName">Last Name: </label>
+                <input type="text" name="lastName" id="lastName"/>
+                </div>
+                <div className="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email"/>
+                </div>
+
+                <div className="form-group">
+                    <label for="message">Message: </label>
+                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+
+
+                </div>
+            <button type="submit">Submit</button>
+
+            </form>
+            </div>
+            </section>
         </div>
     )
 
